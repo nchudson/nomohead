4b6b23fafbce93e3c30f627348de14f4ea299e0a
Nathaniel Hudson <nchudson18@gmail.com>
2021-02-05 14:26:43 -0800

    Remove tunneling capabilities

--- setup.sh
+++ setup.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 echo "Creating config file..."
 
-echo "Enter the location of ngrok: "
-read ngrok_loc
 
 echo "Enter the Dweet ID for IP Address: "
 read d_id_ip
-
-echo "Enter the Dweet ID for Tunnel (leave empty to use the same as IP)"
-read d_id_tun
 
 if [ "$dweet_id_tunnel" = "" ]; then
 	d_id_tun=$d_id_ip
 fi
 
-echo "Enter the delay between posting IP and querying tunnel (Empty for default of 1m): "
-read tun_delay
-
-if [ "$tun_delay" = "" ]; then
-	tun_delay="1m"
-fi
-
-printf "ngrok_location=${ngrok_loc}\ndweet_id_ip=${d_id_ip}\ndweet_id_tunnel=${d_id_tun}\ntunnel_delay=${tun_delay}" > config.cfg
+printf "dweet_id_ip=${d_id_ip}\n" > config.cfg
 
 echo "Adding Cron job..."
 
@@ -29,7 +17,7 @@ DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
 
 command="${DIR}/nomohead.sh"
-job="@reboot bash $command"
+job="*/1 * * * * bash $command"
 echo "Adding to cron - ${job}"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 
